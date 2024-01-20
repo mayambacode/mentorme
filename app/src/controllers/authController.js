8240fc975c4a379bcf7a0f6e827bf83f0b6afba1
Jacob Angga <anggaj@sheridancollege.ca>
2024-01-20 10:42:50 -0500

    setup google SSO

--- app/src/controllers/authController.js
+++ app/src/controllers/authController.js
@@ -1,21 +1,24 @@
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "../firebase";
 import { auth } from '../firebase';
 
-const login = (email, password) => {
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        console.log(userCredential);
-    }).catch((err) => {
-        console.log(err);
-    })
+const authController = {
+    login(email, password) {
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential);
+        }).catch((err) => {
+            console.log(err);
+        })
+    },
+    signup(email, password) {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
 }
 
-const signup = (email, password) => {
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        console.log(userCredential);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-}
+export default authController;
