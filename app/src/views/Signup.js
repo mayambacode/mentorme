f911ac16f74810a26fabfd32d160beab7b0cd918
Mayamba Boka <bokambma@sheridancollege.ca>
2024-01-20 16:43:35 -0500

    bit of css

--- app/src/views/Signup.js
+++ app/src/views/Signup.js
@@ -2,30 +2,30 @@ import { Link } from 'react-router-dom';
 import '../css/Signup.css';
 const Signup = () => {
     return (
-        <div className="SignupLeft">
+        <div className="left">
             
-                <div className="mentorgraphics" id="left">
+                <div className="title">
                     <h1>Welcome to Mentor Me</h1>
                     <h2>Find Your Mentor, Find Your Way</h2>
                 </div>
                
-               <div className="signupform" id="right">
+               <div className="right">
+                    <div className='signupform'>
+                <h1>Sign Up</h1>
 
-               <h1>Sign Up</h1>
+                    <form>
+                        <input type="text" className="fullname" value="Full Name"></input> <br/>
+                    
+                        <input type="email" className="email" value="Email"></input> <br/>
+                        <input type="password" className="password" value="Password"></input><br/> 
+                        <input type="password" className="confirmpassword" value="Confirm Password"></input><br/> 
+                        <inpu type="submit" value="Sign Up"></inpu>
+                    </form>
 
-                 <form>
-                    <input type="text" className="fullname" value="Full Name"></input> <br/>
-                   
-                    <input type="email" className="email" value="Email"></input> <br/>
-                    <input type="password" className="password" value="Password"></input><br/> 
-                    <input type="password" className="confirmpassword" value="Confirm Password"></input><br/> 
-                    <inpu type="submit" value="Sign Up"></inpu>
-                 </form>
-
-                    <div>
-                        <Link href="app\src\views\Login.js">Login</Link>
+                        <div>
+                            <Link href="app\src\views\Login.js">Login</Link>
+                        </div>
                     </div>
-
                 </div>
                
 
